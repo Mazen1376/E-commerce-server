7bdaed2f66da7f3696589833d1f3c6216c711823
mazen <mazen1376797@gmail.com>
2025-08-25 01:52:15 +0300

    category added

--- src/db/models/productModel.js
+++ src/db/models/productModel.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const productSchema = mongoose.Schema({
     name:String,
     description:String,
-    quantity:Number
+    quantity:Number,
+    category:String
 })
 
 export const productModel = mongoose.model('Product', productSchema)
